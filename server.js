6a4d37833a16b734f5fc7eb6401b53290dd05e5d
Vishmita Shetty <vishmita.shetty@tatadigital.com>
2021-05-14 13:22:56 +0530

    fix: fixed express config changes

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/covidresourcesearch'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/covidresourcesearch/'}),
-);
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+//PathLocationStartegy
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
